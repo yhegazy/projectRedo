985966be95c8862dab633300c95702fea2c96c28
thisisnotfez <faaez.kamaal@gmail.com>
2025-02-08 11:12:40 -0700

    Completed Project tasks.

--- src/App.jsx
+++ src/App.jsx
@@ -5,19 +5,23 @@ function App() {
 
   return (
     <>
-      <div class="top-left">
+      <div className="top-left">
         {/* Change the text to your name  */}
-        <h1>
-          Hello Beautiful
-        </h1>
+        <h1>thisisnotfez</h1>
         {/* Fix this CSS so that way these sits under your Name */}
-        <p>Short description of thyself. This doesn't have to be long. 3 to 5 sentances at most.</p>
-        <p>Hobbies. This doesn't have to be long. Maybe make a list?</p>
+        <p>Short description of thyself. This doesn&apos;t have to be long. 3 to 5 sentences at most.</p>
+        <p>I am very uncomfortable with this new way of doing things. Only makes the challenge that much better. I like to eat Haleem.</p>
+        <p>Hobbies. This doesn&apos;t have to be long. Maybe make a list?</p>
+        <ul>
+          <li>3D Printing</li>
+          <li>Vidya Gaym</li>
+          <li>Enjoying the company of my kids</li>
+        </ul>
       </div>
 
-    {/* Change the div's font to 18px and the color to aliceblue */}
-      <div class="center-left">
-        <p>What are your likes and dislikes?</p>  
+      {/* Change the div's font to 18px and the color to aliceblue */}
+      <div className="center-left">
+        <p>What are your likes and dislikes?</p>
       </div>
     </>
   )
